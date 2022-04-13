0fd05ffea5b6c97211bf7f5a2ae8918f85502d03
Yuffi64 <100430798+Yuffi64@users.noreply.github.com>
2022-04-12 22:56:50 -0500

    able to get music array

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import DisplayMusic from './Components/DisplayMusic';
 import MusicTable from './Components/MusicTable';
+// import SearchBar from './Components/SearchBar'
 import './index.css'
 
 
@@ -27,24 +28,32 @@ function App() {
     },
     {
         "id": 3,
-        "title": "I want it that way",
-        "artist": "The Backstreet Boys",
-        "album": "Millennium",
-        "release_date": "1999-04-12",
-        "genre": "pop"
+        "title": "Not Meant To Be",
+        "artist": "Theory of a Deadman",
+        "album": "Scars & souvenirs",
+        "release_date": "2008-12-07",
+        "genre": "Rock"
+    },
+    {
+        "id": 4,
+        "title": "In the end",
+        "artist": "Linkin Park",
+        "album": "Hybird Theory",
+        "release_date": "1999-10-09",
+        "genre": "alternative, hard rock"
     }
 ]);
 
     useEffect(()=>{
-      // getAllMusic();
-    }, [])
+      getAllMusic();
+    })
 
-  // async function getAllMusic(){
-  //   let response = await axios.get('http://127.0.0.1:8000/api/music/');
-  //   setMusicLibrary(response.data);
-
-  //   console.log(response.data); 
-  // }
+    async function getAllMusic(){
+      let response = await axios.get('http://127.0.0.1:8000/api/music/');
+      console.log('getAllMusic Response',response.data);
+      // getAllMusic(response.data); 
+     }
+ 
   
 
 
@@ -58,7 +67,9 @@ function App() {
         <div className='row'>
             <div className='col-md-6em'>
               <h1>Music Library</h1>
+               
               <div className='border-box'>
+                {/* <SearchBar /> */}
                 <MusicTable parentMusic={musicLibrary}/>
               </div>
             </div>
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const SearchBar = (props) => {
+    <div>
+        <form>
+            <input type = 'search' value={''}/>
+        </form>
+            {props.parentMusic.filter((music,index))}
+    </div>
+}
